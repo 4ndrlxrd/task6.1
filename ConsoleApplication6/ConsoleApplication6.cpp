2aee305dff4435e6fbc8b5af23642cb6e6b8a34b
4ndrlxrd <youvags@gmail.com>
2023-03-13 06:55:19 +0100

    first commit

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <iostream>
+
+int main()
+{
+    int i = 0;
+    while (i < 9)
+    {
+        printf("Hello! \n");
+        i = i + 1;
+    }
+    int a = 0;
+    while (a < 15)
+    {
+        std::cout << (a);
+        printf(" ");
+        a = a + 1;
+    }
+    printf("tolko nechetnie chisla");
+    return 0;
+}
+//https://github.com/4ndrlxrd/task6.1
+
